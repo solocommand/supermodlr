082360fc89cb64dbfde1c1060b7c43bbf9fb6d59
Josh Worden <josh.worden@cygnus.com>
2013-03-20 09:28:27 -0500

    Updates to field input wrapper

--- views/supermodlr/default/web/input/wrapper/field/model/field/default.php
+++ views/supermodlr/default/web/input/wrapper/field/model/field/default.php
@@ -1,52 +1,96 @@
-<dl class="dl-horizontal" id="<?=$form_id; ?>__field__<?=$field->path('_'); ?>__container"<?php
+<?php
+
+/**
+* Field model input wrapper
+* 
+* Defines wrapper logic for display of angular form fields based on field inheritance and properties.
+* Loads field label, field, error, and conditions templates.
+* 
+*/
+
+    $inherited = NULL;
+    
     if (!$field->value_isset() && $model->loaded() && isset($model->extends) && $model->extends === NULL || $field->hidden)
-        {
-            echo ' style="display: none"';
-        } 
+    {
+        $style = 'display: none;';
+    } 
+    else
+    {
+        $style = '';
+    }
 
-        ?> >
+?>
 
-        <?= $view->get('label',$field); ?>
+<dl class="dl-horizontal" id="<?=$form_id; ?>__field__<?=$field->path('_'); ?>__container" style="<?= $style ?>">
+
+    <?= $view->get('label',$field); ?>
 
     <dd>
 
 <?php 
-$inherited = NULL;
-if (in_array($field->name, $model->cfg('inherited')) && $model->loaded())
-{
-    $inherited = TRUE;
-}
 
-if (!$field->hidden && !in_array($field->name, $model->cfg('uninherited'))) { 
-    ?><div id="<?=$form_id; ?>__field__<?=$field->path('_'); ?>__inherit" class='<?=$form_id; ?>inherit_container inherit_container'<?php
-    //hide on field create
-    if ($model->is_new() || $model->extends === NULL) {
-        echo " style='display: none'";
-    }?>>Inherit <?php echo $field->name; ?> <input id="<?=$form_id; ?>__field__<?=$field->path('_'); ?>__inheritcb" type="checkbox" <?php
-        if ($inherited || (!isset($field->value) && $model->loaded()))
-            {
-                echo ' checked="checked"';
-            }
-        ?> /></div>
-<script type="text/javascript">
-$('#<?=$form_id; ?>__field__<?=$field->path('_'); ?>__inheritcb').on('change',function(){
-    if ($(this).is(':checked')) {
-        //show field so it can be edited
-        $(this).closest('dd').next('.field').hide();
-    } else {
-        //hide field so it cannot be edited
-        $(this).closest('dd').next('.field').show();
-
-        //unset the scope value
-        var scope = angular.element($('#<?=$form_id; ?>__field__<?=$field->path('_'); ?>')[0]).scope();
-        delete (scope.data.<?php echo $field->get_model_name(); ?>.<?php echo $field->path('.'); ?>);
-
+    if (in_array($field->name, $model->cfg('inherited')) && $model->loaded())
+    {
+        $inherited = TRUE;
     }
 
-});
+    // start field inheritance logic
+    if (!$field->hidden && !in_array($field->name, $model->cfg('uninherited')))
+    { 
 
-</script><?php
-} 
+        // Hide on field create
+        if ($model->is_new() || $model->extends === NULL)
+        {
+            $inherit_style = 'display: none';
+        }
+        else
+        {
+            $inherit_style = '';
+        }
+
+        if ($inherited || (!isset($field->value) && $model->loaded()))
+        {
+            $inherit_checked = ' checked="checked"';
+        }
+        else
+        {
+            $inherit_checked = '';
+        }
+    
+?>
+        <div id="<?=$form_id; ?>__field__<?=$field->path('_'); ?>__inherit" class='<?=$form_id; ?>inherit_container inherit_container' style="<?= $inherit_style; ?>">
+            <label class="checkbox">
+                <input id="<?=$form_id; ?>__field__<?=$field->path('_'); ?>__inheritcb" type="checkbox" <?= $inherit_checked; ?> />
+                Inherit <?= $field->name; ?>?
+            </label>
+        </div>
+
+
+        <script type="text/javascript">
+        $('#<?=$form_id; ?>__field__<?=$field->path('_'); ?>__inheritcb').on('change',function(){
+            if ($(this).is(':checked')) {
+                //show field so it can be edited
+                $(this).closest('dd').next('.field').hide();
+            } else {
+                //hide field so it cannot be edited
+                $(this).closest('dd').next('.field').show();
+
+                //unset the scope value
+                // var scope = angular.element($('#<?=$form_id; ?>__field__<?=$field->path('_'); ?>')[0]).scope();
+                // delete (scope.data.<?php echo $field->get_model_name(); ?>.<?php echo $field->path('.'); ?>);
+                console.log('@todo: unset scope value');
+                console.log('@todo: create directive for field inheritance.')
+
+            }
+
+        });
+
+        </script>
+
+<?php
+
+    } // end field inheritance logic
+
 ?>
 
     </dd>
